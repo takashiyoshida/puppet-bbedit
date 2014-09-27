ee17495a2fc0a58bcf7f12bc71005c2d85afe29a
Takashi Yoshida <takashi@takashiyoshida.org>
2014-09-28 07:56:45 +0800

    Bump version to 10.5.12

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #   include bbedit
 class bbedit {
   package { 'BBEdit':
-    source   => 'http://pine.barebones.com/files/BBEdit_10.5.10.dmg',
+    source   => 'http://pine.barebones.com/files/BBEdit_10.5.12.dmg',
     provider => 'appdmg'
   }
 }
