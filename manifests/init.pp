f0b0f0ac49a967f0bad79b485f107a316f483469
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-17 18:39:51 +0800

    Fix another unit test breakage

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,7 @@ class bbedit (
   $version = '10.5.13',
 ){
   package { 'BBEdit':
+    ensure   => 'installed',
     source   => 'http://pine.barebones.com/files/BBEdit_${version}.dmg',
     provider => 'appdmg'
   }
